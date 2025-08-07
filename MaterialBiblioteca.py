1e71df8360d5c501da16c7046709d1beaa1e324b
NoelGiron <mngiron@hotmail.com>
2025-08-07 16:19:50 -0600

    funcion para generar un codigo de 8 digitos aleatorio

--- MaterialBiblioteca.py
+++ MaterialBiblioteca.py
@@ -1,12 +1,21 @@
 from abc import ABC, abstractmethod
+import random
+import string
 
 class MaterialBiblioteca(ABC):
     def __init__(self, titulo, autor, codigo):
         self.titulo = titulo
         self.autor = autor
-        self.codigo = codigo
+        self.__codigo = codigo
         self.estado = True
-    
+
+    def set_codigoUnico(self):
+        caracteres = string.ascii_letters + string.digits
+        self.__codigo = ''.join(random.choice(caracteres) for _ in range(8))
+
+    def informacionGeneral(self):
+        return f'Titulo: ${self.titulo} Autor: ${self.autor} Disponible: ${self.estado}'
+
     @abstractmethod
     def prestarMaterial(self):
         pass
@@ -14,7 +23,6 @@ class MaterialBiblioteca(ABC):
     def devolverMaterial(self):
         pass
 
-    def informacionGeneral(self):
-        return f'Titulo: ${self.titulo} Autor: ${self.autor} Disponible: ${self.estado}'
+    
 
 
