88d002b79050af8e23e9ef1063dc90ea1b45d870
NoelGiron <mngiron@hotmail.com>
2025-08-07 16:30:45 -0600

    declarada subclase libroFisico

---
+++ LibroFisico.py
@@ -0,0 +1,12 @@
+import MaterialBiblioteca
+
+class libroFisico(MaterialBiblioteca):
+    def __init__(self, titulo, autor, codigo, numeroEjemplar):
+        super().__init__(titulo, autor, codigo)
+        self.numeroEjemplar = numeroEjemplar
+
+    def prestarMaterial(self):
+        pass
+
+    def devolverMaterial(self):
+        pass
