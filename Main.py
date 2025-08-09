cc607ff7e68ab884705e4f12a1b3c9d92fa0ade7
NoelGiron <mngiron@hotmail.com>
2025-08-08 22:52:42 -0600

    se realizo el menu principal con todoas las opciones fixed: #4

---
+++ Main.py
@@ -0,0 +1,59 @@
+
+def Menu():
+    print("<------------------Menu--------------------->")
+    print("< 1) Registrar nuevo Material--------------->")
+    print("< 2) Gestionar los materiales registrados--->")
+    print("< 3) Salir---------------------------------->")
+    print("<------------------------------------------->")
+
+if __name__=="__main__":
+    pregunta = True
+    while pregunta:
+        Menu()
+        opcion = int(input("ingrese una de las opciones: "))
+
+        if opcion == 1:
+            print("1) Registrar libro físico")
+            print("2) Registrar libro digital")
+            print("3) Regresar")
+            opcion = int(input("ingrese una de las opciones: "))
+            
+            if opcion == 1:
+                print("1) Ingrese un nuevo libro fisico")
+            
+            elif opcion == 2:
+                print("2) Ingrese un nuevo libro digital")
+            
+            elif opcion == 3:
+                pass
+
+            else:
+                print("Opción no váldia, intente de nuevo")
+
+        elif opcion == 2:
+            print("1) Prestar libro")
+            print("2) Devolver libro")
+            print("3) Consultar informacion")
+            print("4) Regresar")
+            opcion = int(input("ingrese una de las opciones: "))
+            
+            if opcion == 1:
+                print("Se a prestado un libro")
+            
+            elif opcion == 2:
+                print("Se a devuelto el libro")
+            
+            elif opcion == 3:
+                print("Esta es la informacion del libro")
+
+            elif opcion == 4:
+                pass
+                
+            else:
+                print("Opción no váldia, intente de nuevo")
+
+        elif opcion == 3:
+            break
+
+        else:
+            print("Opción no váldia, intente de nuevo")
