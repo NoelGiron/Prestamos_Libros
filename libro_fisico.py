12e98c5beb8831117cb269e79e762e826dca5b7d
NoelGiron <mngiron@hotmail.com>
2025-08-16 15:24:58 -0600

    se quitaron los get y set de la clase libro fisico

--- libro_fisico.py
+++ libro_fisico.py
@@ -4,30 +4,21 @@ class LibroFisico(MaterialBiblioteca):
     def __init__(self, titulo, autor, codigo, numeroEjemplar):
         super().__init__(titulo, autor, codigo)
         self.numeroEjemplar = numeroEjemplar
-
-    def get(self):
-        return self.numeroEjemplar
-    
-    def set(self, numero):
-        self.numeroEjemplar = numero
+        self.numeroDisponible = numeroEjemplar
 
     def prestarMaterial(self):
-        ejemplarOriginal = self.numeroEjemplar
-        ejemplarActual = ejemplarOriginal - 1
-        if ejemplarActual != -1:
+        if  self.numeroDisponible > 0:
             print(f'Se a prestado el libro: {self.titulo}')
-            self.numeroEjemplar = ejemplarActual
+            self.numeroDisponible -= 1
         else:
             print("Este libro ya no se puede prestar")
 
     def devolverMaterial(self):
-        ejemplarOriginal = self.numeroEjemplar
-        ejemplarActual = ejemplarOriginal + 1
-        if ejemplarActual != self.numeroEjemplar:
+        if self.numeroDisponible < self.numeroEjemplar:
             print(f'Se a regresado el libro: {self.titulo}')
-            self.numeroEjemplar = ejemplarActual
+            self.numeroDisponible += 1
         else:
             print("Este libro ya no se puede devolver")
 
     def __str__(self):
-        return f'Titulo: {self.titulo} Autor: {self.autor} Ejemplares: {self.numeroEjemplar}'
+        return f'Titulo: {self.titulo} Autor: {self.autor} Ejemplares: {self.numeroDisponible}'
